3863cdde3e52e709c91cc79349d1d1fffcc86d4f
daniel_gully <daniel.gully@hm.edu>
2025-07-29 18:12:55 +0200

    Worked on frontend: in page Notizenansicht.vue added functionality to "TODO HINZUFÜGEN" so that new input elements can be added and deleted via the round button next to it. Todos are in scalable undependent lists which turn into a scrollwindow when reached max height.

--- refarch-frontend/src/views/Notizenansicht.vue
+++ refarch-frontend/src/views/Notizenansicht.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row>
       <v-col cols="6">
-        <v-card class="d-flex flex-column justify-between h-100">
+        <v-card>
           <v-card-title class="d-flex justify-between align-center">
             <span>Persönliche ToDos</span>
             <router-link to="/persoenliche-todos" class="ml-auto">
@@ -12,13 +12,35 @@
             </router-link>
           </v-card-title>
           <v-card-actions>
-            <v-btn class="ml-0">ToDo hinzufügen</v-btn>
+            <v-btn @click="addTodo">ToDo hinzufügen</v-btn>
           </v-card-actions>
+          <div class="todo-list">
+            <div
+                v-for="(todo, index) in personalTodos"
+                :key="index"
+                class="todo-item d-flex align-center mb-0"
+            >
+              <v-text-field
+                  v-model="todo.text"
+                  placeholder="Neues ToDo"
+                  dense
+                  hide-details
+                  clearable
+              ></v-text-field>
+              <v-btn
+                  class="delete-button"
+                  @click="deleteTodo(index)"
+                  icon
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </div>
+          </div>
         </v-card>
       </v-col>
 
       <v-col cols="6">
-        <v-card class="d-flex flex-column justify-between h-100">
+        <v-card>
           <v-card-title class="d-flex justify-between align-center">
             <span>Mir zugewiesene GruppenToDos</span>
             <router-link to="/mir-zugewiesene-todos" class="ml-auto">
@@ -28,10 +50,76 @@
             </router-link>
           </v-card-title>
           <v-card-actions>
-            <v-btn class="ml-0">ToDo hinzufügen</v-btn>
+            <v-btn @click="addGroupTodo">ToDo hinzufügen</v-btn>
           </v-card-actions>
+          <div class="todo-list">
+            <div
+                v-for="(todo, index) in groupTodos"
+                :key="index"
+                class="todo-item d-flex align-center mb-0"
+            >
+              <v-text-field
+                  v-model="todo.text"
+                  placeholder="Neues GruppenToDo"
+                  dense
+                  hide-details
+                  clearable
+              ></v-text-field>
+              <v-btn
+                  class="delete-button"
+                  @click="deleteGroupTodo(index)"
+                  icon
+              >
+                <v-icon>mdi-delete</v-icon>
+              </v-btn>
+            </div>
+          </div>
         </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      personalTodos: [],
+      groupTodos: []
+    };
+  },
+  methods: {
+    addTodo() {
+      this.personalTodos.push({ text: '' });
+    },
+    addGroupTodo() {
+      this.groupTodos.push({ text: '' });
+    },
+    deleteTodo(index) {
+      this.personalTodos.splice(index, 1);
+    },
+    deleteGroupTodo(index) {
+      this.groupTodos.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.todo-list {
+  max-height: 453px;
+  overflow-y: auto;
+  margin-top: 16px;
+}
+
+.todo-item {
+  display: flex;
+  align-items: center;
+  margin: 0;
+  width: 100%;
+}
+
+.delete-button {
+  align-items: center;
+}
+</style>
